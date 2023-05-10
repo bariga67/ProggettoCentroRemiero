0a4d73dcd1823c77d0eb5807345c328e1ec182cf
bariga67 <115535754+bariga67@users.noreply.github.com>
2023-05-10 10:51:44 +0000

    definizione pagina principale sito

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ for (var i = 0; i < linkLength; i++) {
     navLinks[i].className = "active";
   }
 }
+const cookieBanner = document.getElementById('cookie-banner');
+const acceptCookies = document.getElementById('accept-cookies');
 
+if (document.cookie.indexOf('cookie_accepted=true') === -1) {
+  cookieBanner.classList.remove('hidden');
+}
+
+acceptCookies.addEventListener('click', function() {
+  document.cookie = 'cookie_accepted=true';
+  cookieBanner.classList.add('hidden');
+});
